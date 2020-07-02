c88d6f8df178269cb21febe3ea86893f7a59581f
Kishor Suryawanshi <suryawanshikishor@hotmail.com>
2020-07-02 13:09:54 +0530

    solution rearranged

---
+++ Entities/Models/SeriesMaster.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SALEERP.Models
+{
+    public partial class SeriesMaster
+    {
+        public SeriesMaster()
+        {
+            MirrorDetails = new HashSet<MirrorDetails>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public DateTime? CreatedDatetime { get; set; }
+        public int? CreatedBy { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedDatetime { get; set; }
+        public bool? IsActive { get; set; }
+
+        public virtual ICollection<MirrorDetails> MirrorDetails { get; set; }
+    }
+}
---
+++ Entities/Models/SpecialEdition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SALEERP.Models
+{
+    public partial class SpecialEdition
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? CreatedDatetime { get; set; }
+        public int? CreatedBy { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedDatetime { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
